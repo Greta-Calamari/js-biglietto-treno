6377be3fd049b9aaef1f489986ec7208648ae6c3
ladykakki <greta_calamari@hotmail.it>
2022-03-24 13:22:39 +0100

    richiesta anni

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
-let numeroKm = (prompt('Quanti chilometri vuoi percorrere?'))
-console.log(numerokm)
+let numeroKm = (prompt('Quanti chilometri vuoi percorrere?'));
+console.log(numerokm);
+
+let userAge (prompt('Quanti anni hai?'))
+console.log(userAge);
+
