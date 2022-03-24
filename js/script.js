be59e5a4e4d9847b6455101146065b3e49d6027c
ladykakki <greta_calamari@hotmail.it>
2022-03-24 15:56:08 +0100

    fx

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 let numeroKm = parseInt (prompt('quanti chilometri vuoi percorrere?'));
 console.log(numeroKm);
 
-let userAge = parseInt (prompt('quanti anni hai?'))
+let userAge = parseInt (prompt('quanti anni hai?'));
 console.log(userAge);
 
 let ticketPrice = (numeroKm * 0.21);
 console.log(ticketPrice);
 
+let scontoSenior = (ticketPrice / 100 * 40);
+let scontoMinore = (ticketPrice / 100 * 20);
+
 if (userAge >= 65){
-    console.log(ticketPrice / 100 * 40 - ticketPrice)
+console.log(ticketPrice - scontoSenior);
 
 }else{
-    console.log(numeroKm * 0.21)
+    console.log(numeroKm * 0.21);
+    
 }if (userAge < 18){
-    console.log(ticketPrice / 100 * 20 - ticketPrice)
+    console.log(ticketPrice - scontoMinore);
 } 
 
-document.getElementById('output').innerHTML = (Math.floor(ticketPrice / 100 * 20 - ticketPrice));
+
+document.getElementById('output').innerHTML = ((ticketPrice - scontoMinore,ticketPrice - scontoSenior ));
